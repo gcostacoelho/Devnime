a0a42f020e228b45ac2a83c5c8d0824b0c83cf01
gcostacoelho <gcostacoelho2003@gmail.com>
2021-09-02 11:00:39 -0300

    Tenth-commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function movie(){
+    window.location.href= "https://www.youtube.com/watch?v=VKyz5SVH1Ug"
+}
+
+function info(){
+    window.location.href = "https://www.themoviedb.org/movie/413594-sword-art-online-the-movie-ordinal-scale?language=pt-BR"
+}
