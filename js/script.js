81d3c2cc6658f29eecdb21e5faf27f92ba72f8b0
gcostacoelho <gcostacoelho2003@gmail.com>
2021-09-09 11:06:53 -0300

    fifteenth-commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ function movie(){
 function info(){
     window.location.href = "https://www.themoviedb.org/movie/413594-sword-art-online-the-movie-ordinal-scale?language=pt-BR"
 }
+
+var btn = document.getElementById('btn-div');
+var container = document.querySelector('.account-box');
+
+btn.addEventListener('click', function(){
+    if (container.style.display === 'flex'){
+        container.style.display = 'none';
+    }else{
+        container.style.display = 'flex';
+    }
+});
